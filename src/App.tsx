e87f604e61edf505d0c226757926d159fa8bf5e6
Elmar Burke <hallo@elmarburke.de>
2018-10-09 12:51:42 +0200

    exercise-50-01-list

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,28 @@ import * as React from 'react';
 import './App.css';
 import Status from './components/Status';
 
+const statusList = [
+  {
+    id: 1,
+    status: 'React ist toll',
+    userName: 'ElmarBurke'
+  },
+  {
+    id: 2,
+    status: 'Das ist eigentlich ganz einfach',
+    userName: 'HCO'
+  }
+];
+
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <h1>Hallo Berlin</h1>
         <hr />
-        <Status status="React ist toll" userName="ElmarBurke" />
-        <Status status="Das ist eigentlich ganz einfach" userName="HCO" />
+        {statusList.map(({ id, status, userName }) => (
+          <Status key={id} status={status} userName={userName} />
+        ))}
       </div>
     );
   }
