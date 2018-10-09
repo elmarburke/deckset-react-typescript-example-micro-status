7ebf6f298f5cff072bffc47058189a5e39d732fb
Elmar Burke <hallo@elmarburke.de>
2018-10-09 13:18:22 +0200

    exercise-60-01-fetch-state

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,31 @@ import * as React from 'react';
 import './App.css';
 import Status from './components/Status';
 
-const statusList = [
-  {
-    id: 1,
-    status: 'React ist toll',
-    userName: 'ElmarBurke'
-  },
-  {
-    id: 2,
-    status: 'Das ist eigentlich ganz einfach',
-    userName: 'HCO'
-  }
-];
+interface IStatus {
+  id: number;
+  status: string;
+  userName: string;
+}
+
+interface IState {
+  statusList: IStatus[];
+}
 
 class App extends React.Component {
+  public state: IState = {
+    statusList: []
+  };
+
+  public async componentDidMount() {
+    const res = await fetch('status.json');
+    const statusList = await res.json();
+
+    this.setState({ statusList });
+  }
+
   public render() {
+    const { statusList } = this.state;
+
     return (
       <div className="App">
         <h1>Hallo Berlin</h1>
