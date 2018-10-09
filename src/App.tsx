650a26434fa38a77c6ff5a1b499de88f284ffef2
Elmar Burke <hallo@elmarburke.de>
2018-10-09 11:16:28 +0200

    exercise-40-01-status-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import "./App.css";
+import Status from "./components/Status";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <h1>Hallo Berlin</h1>
+        <hr />
+        <Status />
       </div>
     );
   }
