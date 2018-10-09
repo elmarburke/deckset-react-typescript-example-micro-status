a9683dcc48b3ebd93c71318c473faaf34ab62fbc
Elmar Burke <hallo@elmarburke.de>
2018-10-09 11:42:53 +0200

    exercise-40-02-status-props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import "./App.css";
-import Status from "./components/Status";
+import * as React from 'react';
+import './App.css';
+import Status from './components/Status';
 
 class App extends React.Component {
   public render() {
@@ -8,7 +8,8 @@ class App extends React.Component {
       <div className="App">
         <h1>Hallo Berlin</h1>
         <hr />
-        <Status />
+        <Status status="React ist toll" userName="ElmarBurke" />
+        <Status status="Das ist eigentlich ganz einfach" userName="HCO" />
       </div>
     );
   }
